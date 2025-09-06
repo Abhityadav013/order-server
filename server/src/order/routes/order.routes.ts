ff0183ba0fb1ab67ed95e5c53395208cfa3cd86c
Abhit Yadav <abhityadav013@gmail.com>
2025-09-06 14:33:38 +0300

    get order by Id

--- server/src/order/routes/order.routes.ts
+++ server/src/order/routes/order.routes.ts
@@ -1,11 +1,13 @@
-import { Router } from 'express';
-import { OrderController } from '../controller/order.controller';
-
+import { Router } from "express";
+import { OrderController } from "../controller/order.controller";
 
 const router = Router();
 const controller = new OrderController();
 
 // This is the webhook endpoint that replaces Next.js API route
-router.post('/', (req, res, next) =>controller.createOrder(req, res, next));
+router.post("/", (req, res, next) => controller.createOrder(req, res, next));
+router.get("/", (req, res, next) =>
+  controller.getOrderByOrderId(req, res, next)
+);
 
 export default router;
