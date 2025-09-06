35d4d938520032faa865a57c87e49a4323199033
Abhit Yadav <abhityadav013@gmail.com>
2025-09-06 12:07:58 +0300

    fix: update order route to use root path for order creation

--- server/src/order/routes/order.routes.ts
+++ server/src/order/routes/order.routes.ts
@@ -6,6 +6,6 @@ const router = Router();
 const controller = new OrderController();
 
 // This is the webhook endpoint that replaces Next.js API route
-router.post('/order', (req, res, next) =>controller.createOrder(req, res, next));
+router.post('/', (req, res, next) =>controller.createOrder(req, res, next));
 
 export default router;
