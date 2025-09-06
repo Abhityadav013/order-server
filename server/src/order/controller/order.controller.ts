e58a04b73c14f81c164177ad78f9a2bbd2f9c484
Abhit Yadav <abhityadav013@gmail.com>
2025-09-06 14:39:19 +0300

    add swagger annotation for fetch and create order

--- server/src/order/controller/order.controller.ts
+++ server/src/order/controller/order.controller.ts
@@ -4,6 +4,57 @@ import { OrderService } from "../service/order.service";
 const service = new OrderService();
 
 export class OrderController {
+  /**
+   * @swagger
+   * /order:
+   *   post:
+   *     summary: Create a new order
+   *     tags:
+   *       - Orders
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             description: Order creation payload
+   *             properties:
+   *               selectedMethod:
+   *                 type: string
+   *                 enum: [CASH, CARD, PAYPAL, GOOGLE_PAY, APPLE_PAY] # example
+   *               basketId:
+   *                 type: string
+   *               orderType:
+   *                 type: string
+   *                 enum: [PICKUP, DELIVERY]
+   *               deliveryTime:
+   *                 type: object
+   *                 properties:
+   *                   asap:
+   *                     type: boolean
+   *                   scheduledTime:
+   *                     type: string
+   *                     format: date-time
+   *               deliveryNote:
+   *                 type: string
+   *                 nullable: true
+   *     responses:
+   *       201:
+   *         description: Order created successfully
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 message:
+   *                   type: string
+   *                 data:
+   *                   $ref: '#/components/schemas/OrderSuccessSummary'
+   *       400:
+   *         description: Bad request
+   *       500:
+   *         description: Internal server error
+   */
   async createOrder(req: Request, res: Response, next: NextFunction) {
     try {
       const deviceId = (req as any).deviceId;
@@ -17,7 +68,35 @@ export class OrderController {
     }
   }
 
-    async getOrderByOrderId(req: Request, res: Response, next: NextFunction) {
+  /**
+   * @swagger
+   * /order/{orderId}:
+   *   get:
+   *     summary: Get order details by orderId
+   *     tags:
+   *       - Orders
+   *     parameters:
+   *       - in: path
+   *         name: orderId
+   *         required: true
+   *         schema:
+   *           type: string
+   *         description: Unique order ID
+   *     responses:
+   *       200:
+   *         description: Order details retrieved successfully
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/OrderSuccessSummary'
+   *       400:
+   *         description: Missing orderId parameter
+   *       404:
+   *         description: Order not found
+   *       500:
+   *         description: Internal server error
+   */
+  async getOrderByOrderId(req: Request, res: Response, next: NextFunction) {
     try {
       const { orderId } = req.params;
 
